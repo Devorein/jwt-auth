8d25f3bd0a821e483cf0aa570e97e3137d2274fe
Safwan Shaheer <devorein00@gmail.com>
2021-05-01 20:58:36 +0600

    Added register form

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 
 function Routes() {
   return <BrowserRouter>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
+    </div>
     <Switch>
       <Route path="/" exact render={() => <Home />} />
       <Route path="/register" exact render={() => <Register />} />
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,3 +1,34 @@
+import { useState } from "react";
+import { UserDataInput } from "../generated/graphql";
+
 export const Register = () => {
-  return <div>Register</div>;
+  const [registerData, setRegisterData] = useState<UserDataInput>({
+    email: '',
+    password: '',
+    username: ''
+  });
+
+  return <form onSubmit={(e) => {
+    e.preventDefault();
+    console.log(registerData)
+  }}>
+    <div>
+      <input placeholder="Email" value={registerData.email} onChange={e => setRegisterData({
+        ...registerData,
+        email: e.target.value
+      })} />
+    </div>
+    <div>
+      <input placeholder="Username" value={registerData.username} onChange={e => setRegisterData({
+        ...registerData,
+        username: e.target.value
+      })} />
+    </div>
+    <div>
+      <input placeholder="Password" type="password" value={registerData.password} onChange={e => setRegisterData({
+        ...registerData,
+        password: e.target.value
+      })} />
+    </div>
+    <button type="submit">Submit</button></form>;
 }
